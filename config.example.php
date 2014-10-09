c5bbc794150fa4d2ce7d7b7f5a1dbb0137bf5582
DanyaPostfactum <danya.postfactum@gmail.com>
2014-10-09 17:16:22 +1000

    ignore config.php

---
+++ config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+$cd = basename(dirname(__FILE__));
+
+if ($cd == 'newdnar') {
+   $directory = '/newdnar/';
+} else {
+   $directory = '/';
+}
+
+define("BASE_URL", "$directory");
+define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "$directory");
+?>
